17e5dfa08aaab153bddc7ade1b822e3e60d086ba
Devin Drawhorn <devin@drawhorn.com>
2018-08-24 13:48:50 -0500

    Added runner class using Apache Commons CLI Fixes #4

---
+++ src/main/java/PlayWikIRMan.java
@@ -0,0 +1,36 @@
+import org.apache.commons.cli.*;
+
+public class PlayWikIRMan {
+
+    public static void main(String[] args) {
+        ExtractorOptions eo = ExtractorOptions.INFO_TABLE;
+        OutputOptions oo = OutputOptions.TERM_TEXT;
+        Options options = new Options();
+        options.addOption("help", false, "prints this message");
+        options.addOption("asciiArt", false, "print out ascii art after each round");
+        options.addOption("fromBody", false, "extract word bank from the html body of Wikipedia");
+        options.addOption("hint", true, "how many correct guesses before a hint is displayed");
+        options.addOption("turns", true, "how many turns you have per round");
+        CommandLine cmd = null;
+        try {
+            cmd = new DefaultParser().parse( options, args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.out.println("Error parsing command line arguments");
+            System.exit(-1);
+        }
+        if (cmd.hasOption("help")) {
+            new HelpFormatter().printHelp("WikIRMan [options]", options);
+        } else {
+            if (cmd.hasOption("fromBody")) {
+                eo = ExtractorOptions.BODY;
+            }
+            if (cmd.hasOption("asciiArt")) {
+                oo = OutputOptions.TERM_ASCII_ART;
+            }
+            new GameLoop(eo, oo, Integer.parseInt(cmd.getOptionValue("t", "7")),
+                    Integer.parseInt(cmd.getOptionValue("h", "0")));
+        }
+    }
+
+}
